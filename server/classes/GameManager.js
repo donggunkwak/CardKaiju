284c3db07b55828b57d487343d72f60058971ba5
Kevin Kwak <donggunkwak@gmail.com>
2025-05-24 23:12:38 -0700

    fully added support for joining and leaving rooms

--- server/classes/GameManager.js
+++ server/classes/GameManager.js
@@ -29,7 +29,7 @@ class GameManager{
         if(code.length!=CODELENGTH){
             throw Error("Code needs to be length of 6!");
         }
-        this.rooms[code] = new Room(code);
+        return this.rooms[code] = new Room(code);
     }
     deleteRoom(code){
         const room = this.rooms[code];
@@ -42,15 +42,17 @@ class GameManager{
         if(!Object.keys(this.players).includes(uuid))
             throw Error(`Player with uid ${uuid} not found!`);
         
+        var room = this.rooms[code];
+        if(code==null||!Object.keys(this.rooms).includes(code))//create room if it doesn't exist
+            room = this.createRoom(code);
 
-        if(!Object.keys(this.rooms).includes(code))//create room if it doesn't exist
-            this.createRoom(code);
-        
         const player =this.players[uuid];
-        const room = this.room[code];
         if(room.hasSpace()){
             room.addPlayer(player);//try to add player to the room first to see if it works!
-            player.roomCode = code;
+            player.roomCode = room.id;
+        }
+        else{
+            throw Error("This room is full!");
         }
     }
     leaveRoom(uuid){
@@ -58,11 +60,15 @@ class GameManager{
             throw Error(`Player with uid ${uuid} not found!`);
 
         const player =this.players[uuid];
-        if(player.roomCode==null)
+        const roomCode = player.roomCode;
+
+        if(roomCode==null)
             throw Error("Player is not in a room!");
-        const room = this.rooms[player.roomCode];
+        const room = this.rooms[roomCode];
         room.removePlayer(player);
         player.roomCode=null;
+        if(room.isEmpty())
+            this.deleteRoom(roomCode);
     }
 
     handleMessage(uuid, message){
@@ -79,10 +85,11 @@ class GameManager{
         switch(message.type){
             case 'joinRoom':
                 try{
-                    var code = message.roomCode;
-                    if(!code)
-                        code = null;
+                    var code = null;
+                    if(message.roomCode!=undefined)
+                        code = message.roomCode;
                     this.joinRoom(uuid,code);
+                    playerConnection.send(JSON.stringify({ type: 'message', message: `Succesfully joined room ${this.players[uuid].roomCode}` }));
                 }
                 catch(e){
                     console.log(e);
@@ -92,14 +99,15 @@ class GameManager{
             case 'leaveRoom':
                 try{
                     this.leaveRoom(uuid);
+                    playerConnection.send(JSON.stringify({ type: 'message', message: `Succesfully left room` }));
                 }
                 catch(e){
+                    console.log(e);
                     playerConnection.send(JSON.stringify({ type: 'error', message: e.toString() }));
                 }
                 break;
         }
-        console.log(`Rooms: ${this.rooms}`);
-        console.log(`Players: ${this.players}`);
+        this.printRoomsAndPlayers();
     }
 
     generate_unique_code(){
@@ -115,6 +123,22 @@ class GameManager{
             }
         }
         throw Error("Could not generate a unique code for some reason, try again");
+    }
+
+    printRoomsAndPlayers(){
+        
+        Object.keys(this.rooms).forEach(code=>{
+            const room = this.rooms[code];
+            var curString = `Room:${code}: [`;
+            room['players'].forEach((player)=>{
+                curString+=player['username']+', ';
+            });
+            console.log(curString+"]");
+        });
+        Object.keys(this.players).forEach(uuid=>{
+            const player = this.players[uuid];
+            console.log(`${player.username} - ${player.roomCode}`);
+        })
     }
     
 
